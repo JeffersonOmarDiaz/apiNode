06d2eae9cc5046b5b07ff47e50cd0cf5599898a8
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2022-09-29 23:39:44 -0500

    creacion de los componentes necesarios para el formulario

--- CrudBasico/src/app/app.module.ts
+++ CrudBasico/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarComponent } from './componentes/agregar/agregar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ModificarComponent } from './componentes/modificar/modificar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent,
+    InicioComponent,
+    ModificarComponent
   ],
   imports: [
     BrowserModule,
